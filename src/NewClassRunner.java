6f0827bdd642cc9ae0ee14437ba20d7b2948c2b2
Chris Reilly <Chris Reilly@Mulchbutler-PC.wpa.txstate.edu>
2015-04-07 14:52:39 -0500

    Added some more getters and made NewClassRunner a little easier to read

--- src/NewClassRunner.java
+++ src/NewClassRunner.java
@@ -14,17 +14,41 @@ public class NewClassRunner
     public static void main(String[] args)
     {
         RARDocument doc = new RARDocument();
+        System.out.println("RARDocument created, adding new event.");
         doc.getFutureEvents().add(new Event("Class on WednesDay"));
-        boolean b = doc.getFutureEvents().contains(new Event("Class on WednesDay"));
-        doc.createEvent("Class on WednesDay");
-        doc.getFutureEvents().get(doc.getFutureEvents().indexOf(new Event("Class on WednesDay"))).addInvite(new Member(1234, "Steven", "Claggett"));
-        System.out.println(doc.getFutureEvents().get(doc.getFutureEvents().indexOf(new Event("Class on WednesDay"))).toString());
-        Event ref = doc.getFutureEvents().get(doc.getFutureEvents().indexOf(new Event("Class on WednesDay")));
-        ref.getAttendees().getMember(1234).setfName("Poop");
         
-        doc.getFutureEvents().get(doc.getFutureEvents().indexOf(new Event("Class on WednesDay"))).MemberAttended(new Member(1234));
-        System.out.println(doc.getFutureEvents().get(doc.getFutureEvents().indexOf(new Event("Class on WednesDay"))).toString());
-        System.out.println(doc.getFutureEvents().get(doc.getFutureEvents().indexOf(new Event("Class on WednesDay"))).getAttendees().getMember(1234));
+        ////////////////////////////////////////////////////////////////////////
+        
+        System.out.println("Event Added. Checking to confirm event added");
+        boolean b = doc.getFutureEvents().contains(new Event("Class on WednesDay"));
+        if(b)
+            System.out.println("Event succeddfully added.");
+        else
+            System.out.println("Event not successfully added");
+        
+        ////////////////////////////////////////////////////////////////////////
+        
+        System.out.println("Testing createEvent with previous event. Should fail.");
+        doc.createEvent("Class on WednesDay");
+        ////////////////////////////////////////////////////////////////////////
+        
+        System.out.println("Adding a member to the event, then printing event.");
+        doc.getFutureEvents("Class on WednesDay").addInvite(new Member(1234, "Steven", "Claggett"));
+        System.out.println(doc.getFutureEvents("Class on WednesDay").toString());
+        
+        ////////////////////////////////////////////////////////////////////////
+        
+        System.out.println("Attempting to set the fName of an attendee.");
+        Event ref = doc.getFutureEvents("Class on WednesDay");
+        ref.getInvites().getMember(1234).setfName("Poop");
+        
+        ////////////////////////////////////////////////////////////////////////
+        
+        System.out.println("Setting an attendee as attended. Then printing both event and Member");
+        doc.getFutureEvents("Class on WednesDay").MemberAttended(new Member(1234));
+        System.out.println(doc.getFutureEvents("Class on WednesDay").toString());
+        System.out.println(doc.getFutureEvents("Class on WednesDay").getAttendees().getMember(1234));
+        
         /*DataModel.Member m = new DataModel.Member(1234, "Steven", "Claggett");
         DataModel.MemberSet ms = new DataModel.MemberSet();
         ms.addMember(1234, "This", "Guy");
